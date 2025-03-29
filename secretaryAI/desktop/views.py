c2ec75d3218a3a14502bd5e50a44386345f3295c
AnujinEnkhee <anujinenkhee107@gmail.com>
2025-03-29 18:29:02 -0400

    updated templates folder

--- secretaryAI/desktop/views.py
+++ secretaryAI/desktop/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'desktop/home.html')
 
 def calendar(request):
-    return render(request, 'calendar.html')
+    return render(request, 'desktop/calendar.html')
 
 def daily_schedule(request):
-    return render(request, 'daily_schedule.html')
+    return render(request, 'desktop/daily_schedule.html')
 
 def tasks(request):
-    return render(request, 'tasks.html')
+    return render(request, 'desktop/tasks.html')
 
 def notifications(request):
-    return render(request, 'notifications.html')
+    return render(request, 'desktop/notifications.html')
 
 def profile(request):
-    return render(request, 'profile.html')
+    return render(request, 'desktop/profile.html')
 
 def settings(request):
-    return render(request, 'settings.html')
+    return render(request, 'desktop/settings.html')
